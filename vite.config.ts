1cd50c6287042de4c996a618e9f239dd3d020b5d
Gaia-commons-council <213270779+cannaplan@users.noreply.github.com>
2026-03-18 18:31:06 -0500

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    fs: {
-      allow: ['.']
-    }
-  }
-});
+  base: '/',
+  server: { port: 3000 },
+  build: { outDir: 'dist' }
+})
