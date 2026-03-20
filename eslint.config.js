6a4300670644a94a31418790cabb0f8073b7834a
Gaia-commons-council <213270779+cannaplan@users.noreply.github.com>
2026-03-20 03:13:52 -0500

    Add ESLint configuration for JavaScript and React
    Set up ESLint configuration with recommended settings for JavaScript and React.

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import js from '@eslint/js';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+
+export default [
+  { languageOptions: { globals: js.globals.recommended } },
+  js.configs.recommended,
+  {
+    plugins: {
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+    },
+  },
+];
